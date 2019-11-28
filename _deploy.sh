976e43c427792b7ef3e3490caddce18243217878
Korovin <Vladimir.Korovin@gfk.com>
2019-11-28 21:09:13 +0300

    test

--- _deploy.sh
+++ _deploy.sh
@@ -8,8 +8,8 @@ set -e
 git config --global user.email "dimcoker@yandex.ru"
 git config --global user.name "vrkrv"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-out
+cd book-out
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
