0d2594000a7471fd2aef63893a6870d02b88cc45
Korovin <Vladimir.Korovin@gfk.com>
2019-11-28 21:19:06 +0300

    fix deploy

--- _deploy.sh
+++ _deploy.sh
@@ -8,8 +8,11 @@ set -e
 git config --global user.email "dimcoker@yandex.ru"
 git config --global user.name "vrkrv"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-out
-cd book-out
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+cd book-output
+git rm -rf *
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
