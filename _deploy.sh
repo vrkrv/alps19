33a0c62927a65b91614d4ed902c8a37f13890adf
Korovin <Vladimir.Korovin@gfk.com>
2019-11-28 21:02:23 +0300

    change email

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-#git config --global user.email "dimcoker@yihui.name"
-#git config --global user.name "Yihui Xie"
+git config --global user.email "dimcoker@yandex.ru"
+git config --global user.name "vrkrv"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
