a0fdc43621652a470068f4c78d09881e4504c7b4
vrkrv <dimcoker@yandex.ru>
2019-12-16 00:32:56 +0300

    Add day 12

--- _clean_track.R
+++ _clean_track.R
@@ -247,3 +247,61 @@ m %>%
 # addPopupImages(sprintf("https://yadi.sk/d/ZibKr3f-ZVSunQ/%s", photo_day1$FileName), group = "day1", width = 640, height = 480)
 # addPopupImages(sprintf(yadisk, photo_day1$FileName), group = "day1", width = 640, height = 480)
 # addPopupImages(photo_day1 %>% transmute(FileName, tmp = glue::glue(yadisk)) %>% pull(tmp), group = "day1", width = 640, height = 480)
+
+
+# Mini-track --------------------------------------------------------------
+
+  photo_df2 <- read_tsv("photo_tech_info2.tsv")
+  gpx_file <- "../track/Alps2019_MGU_clean.gpx"
+  wp <- readOGR(gpx_file, layer = "track_points")
+  track_df <- wp %>% as.data.frame %>% as.tbl %>% mutate_at("time", ymd_hms)
+
+
+  day_n <- ymd('2019-07-25')
+
+  photo_day1 <-
+    photo_df2 %>%
+    filter(CreateDate > day_n,
+           CreateDate < day_n + 1)
+  track_day1 <-
+    track_df %>%
+    filter(time > day_n,
+           time < day_n + 1) %>%
+    mutate_at("time", `+`, 7800)
+  sp_track <-  sp::Lines(list(sp::Line(track_day1 %>% select(coords.x1, coords.x2) %>% as.matrix)), "day1")
+
+  # wp_tracks <- readOGR(gpx_file, layer = 'tracks')
+  # yadisk <- "https://getfile.dokpub.com/yandex/get/https://yadi.sk/d/ZibKr3f-ZVSunQ/%s"
+  m <-
+    leaflet() %>%
+    addProviderTiles("OpenTopoMap", group = "OpenTopoMap") %>%
+    addLegend(position = 'bottomright',
+              opacity = 0.4,
+              colors = 'blue',
+              labels = 'Пройденный маршрут',
+              title = 'Поход 2 к.с. Пеннинские Альпы')
+  m %>%
+    addPolylines(
+      data = sp_track,
+      color = 'blue',
+      group = 'day1')  %>%
+    addMarkers(
+      data = photo_day1 %>%
+        mutate_at(vars(coords.x1, coords.x2), list(~ifelse(lag(.) == ., jitter(., factor = 100), .))) %>%
+        mutate(lab = sprintf("%s<br/>N %s, E %s<br/>elevation: %s<br/>%s", time, round(coords.x2, 4), round(coords.x1, 4), ele, FileName)),
+      # data = photo_day1,
+      lng  = ~coords.x1,
+      lat  = ~coords.x2,
+      # label = ~sprintf("%s<br/>N %s, E %s; elevation: %s", time, coords.x1, coords.x2, ele)),
+      label = ~lapply(lab, htmltools::HTML),
+      group = "day1") %>%
+    # addPopupImages(sprintf("../photo_day1$SourceFile, group = "day1", width = 480)
+    addPopupImages(sprintf("../photo/preview/%s", photo_day1$FileName), group = "day1", width = 480)
+  # addPopupImages(sprintf("https://yadi.sk/d/ZibKr3f-ZVSunQ/%s", photo_day1$FileName), group = "day1", width = 640, height = 480)
+  # addPopupImages(sprintf(yadisk, photo_day1$FileName), group = "day1", width = 640, height = 480)
+  # addPopupImages(photo_day1 %>% transmute(FileName, tmp = glue::glue(yadisk)) %>% pull(tmp), group = "day1", width = 640, height = 480)
+
+  track_day1 %>%
+    ggplot(aes(time, ele)) +
+    geom_point() +
+    scale_x_datetime(date_breaks =  "1 hour", date_labels = "%H:%M")
